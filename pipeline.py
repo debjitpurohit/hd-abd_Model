4a3d23781dfa85576c2ebaa3b932f879c5835c14
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2026-04-29 10:44:39 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,34 +1,3 @@
-# from object_detection import detect_objects
-# from lane_detection import detect_lanes
-# from weather import detect_weather
-# from sign_detection import detect_signs
-
-# def to_python(obj):
-#     if isinstance(obj, dict):
-#         return {k: to_python(v) for k, v in obj.items()}
-#     elif isinstance(obj, list):
-#         return [to_python(i) for i in obj]
-#     elif isinstance(obj, tuple):
-#         return [int(i) for i in obj]  
-#     elif hasattr(obj, "item"):  
-#         return obj.item()  
-#     else:
-#         return obj
-
-# def process_frame(frame):
-#     objects = detect_objects(frame)
-#     lanes = detect_lanes(frame)
-#     weather = detect_weather(frame)
-#     signs = detect_signs(frame)
-
-#     result = {
-#         "objects": objects,
-#         "lanes": lanes,
-#         "weather": weather,
-#         "signs": signs
-#     }
-
-#     return to_python(result) 
 from object_detection import detect_objects
 from lane_detection import detect_lanes
 from weather import detect_weather
